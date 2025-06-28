c351277e2c4d459319f8f579f0060c6dada20b07
kilic1978 <kilicmustafa1978@gmail.com>
2025-06-28 22:06:34 +0300

    colors

--- src/week10/Colors.java
+++ src/week10/Colors.java
@@ -7,7 +7,7 @@ import java.util.List;
 
     public class Colors {
         public static void main(String[] args) {
-            ArrayList<String> colors = new ArrayList<>(List.of("brown","black","yellow","pink","white","silver","purple"));
+            ArrayList<String> colors = new ArrayList<>(List.of("brown","green","yellow","pink","white","silver","purple"));
             for (int i = 0; i < colors.size(); i++) {
                 System.out.println(colors.get(i));
             }
