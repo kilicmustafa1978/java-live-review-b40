05d9a887362a878649375f6ece98cd3b08317e03
kilic1978 <kilicmustafa1978@gmail.com>
2025-06-28 22:00:19 +0300

    colors

---
+++ src/week10/Colors.java
@@ -0,0 +1,21 @@
+package week10;
+
+
+
+    import java.util.ArrayList;
+import java.util.List;
+
+    public class Colors {
+        public static void main(String[] args) {
+            ArrayList<String> colors = new ArrayList<>(List.of("brown","black","yellow","pink","white","silver","purple"));
+            for (int i = 0; i < colors.size(); i++) {
+                System.out.println(colors.get(i));
+            }
+            //remove colors that are less than 6 characters:
+            colors.removeIf(color -> color.length() < 6);
+            System.out.println("colors = " + colors);
+
+            colors.forEach(color -> System.out.println(color.toUpperCase()));
+        }
+    }
+
